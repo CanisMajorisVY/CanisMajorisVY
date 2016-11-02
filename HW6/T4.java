58f1cd5d1448f2928e0254da1aefec6913f87d31
Kiku Mihail <CanisMajorisVY@yandex.ru>
2016-11-03 02:23:08 +0300

    HW

--- HW6/T4.java
+++ HW6/T4.java
@@ -1,12 +1,34 @@
 import java.util.Scanner;
 
-public class T3 {
+public class T4 {
+	
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int w = 0;
+		boolean q = false;
+		String s = sc.nextLine();
+		String s1 = "";
+		
+		for (int i = 0; i < s.length(); i ++) {
+			if ((s.charAt(i) == '(')) {
+				w += 1;
+			}
+			else if (s.charAt(i) == ')') {
+				w -= 1;
+			}
+			
+			if (w < 0) q = true;
+		}
+		
+		if ((q)||(w != 0)) {
+			System.out.println("wrong");
+		} 
+		else {
+			System.out.println("true");
+		}
 		
 		
-		
-		
-		
+		System.out.println(s1);
 		
 	}
 }
